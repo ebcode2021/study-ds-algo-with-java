7be8c13208ee4d9f10dce9e13a5142cf565faee6
ebcode2021 <ebcode2021@gmail.com>
2023-12-03 20:16:01 +0900

    [add] streamApi

---
+++ DataStructure/java/streamApiCompare.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class streamApiCompare {
+    public static void main(String[] args) {
+        List<Double> numbers = Arrays.asList(49.1, 25.5, 9.9);
+        
+        // lambda
+        numbers.stream() // 스트림 API
+                .map(n -> Math.round(n))
+                .forEach(n -> System.out.println(n));
+
+        // 메소드 참조
+        numbers.stream() // 스트림 API
+                .map(Math::round)
+                .forEach(System.out::println);
+    }
+    
+
+}
+
+
