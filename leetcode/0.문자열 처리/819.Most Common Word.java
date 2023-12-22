296b4723d53743b4a3ceac39f6254e428c453dbf
ebcode2021 <ebcode2021@gmail.com>
2023-12-22 16:46:54 +0900

    [retry] regex, Collections

---
+++ leetcode/0.문자열 처리/819.Most Common Word.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+	public String mostCommonWord(String paragraph, String[] banned) {
+		Set<String> ban = new HashSet<>(Arrays.asList(banned));
+		Map<String, Integer> counts = new HashMap<>();
+		
+		String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
+
+		for(String w : words) {
+			if (!ban.contains(w)) {
+				counts.put(w, counts.getOrDefault(w, 0) + 1);
+			}
+		}
+		return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
+	}
+}
