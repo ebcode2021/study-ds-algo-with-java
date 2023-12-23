ef89b4f4fb0622976693222c980ddd0bec08f341
ebcode2021 <ebcode2021@gmail.com>
2023-12-23 21:13:58 +0900

    [retry] Anagrams

---
+++ leetcode/0.문자열 처리/49.Group Anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+		Map<String, List<String>> results = new HashMap<>();
+
+		for (String str : strs) {
+			char[] chars = str.toCharArray();
+			Arrays.sort(chars);
+
+			String key = String.valueOf(chars);
+
+			if (!results.containsKey(key))
+				results.put(key, new ArrayList<>());
+			results.get(key).add(str);
+		}
+		return new ArrayList<>(results.values());	
+	}
+}
