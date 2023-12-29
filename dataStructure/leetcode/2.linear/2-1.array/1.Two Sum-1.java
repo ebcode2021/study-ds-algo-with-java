3b6d90e892757c5c77ea77d80df6fa8a7d07ecee
ebcode2021 <ebcode2021@gmail.com>
2023-12-29 21:07:13 +0900

    [solve] brute force

---
+++ dataStructure/leetcode/2.linear/2-1.array/1.Two Sum-1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+/* 1.Brute Force 풀이. O(n^2) */
+class Solution {
+     public int[] twoSum(int[] nums, int target) {
+		int arraySize = nums.length;
+
+		for (int i = 0; i < arraySize; i++) {
+			for (int j = i + 1; j < arraySize; j++) {
+				if (nums[i] + nums[j] == target)
+					return new int[]{i, j};
+			}
+		}
+		return new int[]{};
+     }
+};
