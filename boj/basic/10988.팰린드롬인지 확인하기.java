3b9f46780303a7b4c30769039dce1fbacba46bd3
ebcode2021 <ebcode2021@gmail.com>
2024-01-18 20:43:01 +0900

    [solve] string 기본

--- boj/basic/10988.팰린드롬인지 확인하기.java
+++ boj/basic/10988.팰린드롬인지 확인하기.java
@@ -1,3 +1,20 @@
-public class 10988.팰린드롬인지 확인하기 {
+import java.io.*;
+
+class test {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
+		String word = br.readLine();
+		int wordLen = word.length();
+		int compareCnt = wordLen / 2;
+		int answer = 1;
+
+		for (int i = 0; i < compareCnt; i++) {
+			if (word.charAt(i) != word.charAt(wordLen - 1 - i)) {
+				answer = 0;
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
 }
--- boj/basic/27866.문자와 문자열.java
+++ boj/basic/27866.문자와 문자열.java
@@ -1,3 +1,11 @@
-public class 27866.문자와 문자열 {
-	
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+		int idx = Integer.parseInt(br.readLine());
+
+		System.out.println(word.charAt(idx - 1));
+	}
 }
