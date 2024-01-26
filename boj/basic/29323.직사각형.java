3cfbcae750d9bc3ca09d899aaffbf966b383d1f7
ebcode2021 <ebcode2021@gmail.com>
2024-01-26 19:15:12 +0900

    [solve] boj 도형문제

--- boj/basic/29323.직사각형.java
+++ boj/basic/29323.직사각형.java
@@ -1,20 +1,13 @@
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int length = Integer.parseInt(br.readLine());
+		int height = Integer.parseInt(br.readLine());
+
+		System.out.println(length * height);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int rectX = Integer.parseInt(st.nextToken());
-		int rectY = Integer.parseInt(st.nextToken());
-
-		int minX = Math.abs(x - rectX) < x ? Math.abs(x - rectX) : x;
-		int minY = Math.abs(y - rectY) < y ? Math.abs(y - rectY) : y;
-		System.out.println(minX < minY ? minX : minY);
-
-		br.close();
 	}
 }
