1f81d206be69178d3c9c438274fd3c0d96dd7962
ebcode2021 <ebcode2021@gmail.com>
2024-02-02 18:49:02 +0900

    [retyr] bigInteger..!

--- boj/class2/1676.팩토리얼 0의 개수.java
+++ boj/class2/1676.팩토리얼 0의 개수.java
@@ -1,3 +1,27 @@
-public class 1676.팩토리얼 0의 개수 {
-	
+import java.io.*;
+import java.math.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+
+		BigInteger big = new BigInteger("1");
+		for (int i = 1; i <= num; i++) {
+			big = big.multiply(BigInteger.valueOf(i));
+		}
+
+		String str = big.toString();
+		int res = 0;
+		for (int i = str.length() - 1; i >= 0; i--) {
+			if (str.charAt(i) == '0') {
+				res += 1;
+			} else {
+				break;
+			}
+				
+		}
+		System.out.println(res);
+	}
 }
