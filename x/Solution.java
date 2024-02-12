1226110fa484ab5cf814ecd5a3bffc13604ff5e9
ebcode2021 <ebcode2021@gmail.com>
2024-02-12 21:02:36 +0900

    [retry] 평소 헷갈리는 부분들이 많이 나와서 다시 풀어봤음 좋겠음! (맞은문제)

--- x/Solution.java
+++ x/Solution.java
@@ -1,29 +1,25 @@
 import java.util.*;
 
 class Solution {
-	
-	public void dfs(List<List<Integer>> results, int[] candidates, int target, int index, Deque<Integer> path) {
+	private static void dfs(List<List<Integer>> results, int[] nums, int index, Deque<Integer> path) {
+		// 모든 탐색 경로를 매번 결과에 추가
+		results.add(new ArrayList<>(path));
 
-		if (target < 0) {
-			return ;
-		}
-		if (target == 0) {
-			results.add(new ArrayList<>(path));
-			return ;
-		}
-
-		for (int i = index; i < candidates.length; i++) {
-			path.add(candidates[i]);
-			dfs(results, candidates, target - candidates[i], i, path);
+		// 자기 자신보다 큰 숫자를 DFS 진행
+		for (int i = index; i < nums.length; i++) {
+			// path에 현재 엘리먼트 추가
+			path.add(nums[i]);
+			// 재귀 DFS
+			dfs(results, nums, i + 1, path);
+			// 돌아온 이후에는 현재 엘리먼트 삭제
 			path.removeLast();
 		}
 	}
-
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+	
+	public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> results = new ArrayList<>();
 
-		dfs(results, candidates, target, 0, new ArrayDeque<>());
-
+		dfs(results, nums, 0, new ArrayDeque<>());
 		return results;
 	}
 }
