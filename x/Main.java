110e5e993968fb7ac210488255d31656a656d5ac
ebcode2021 <ebcode2021@gmail.com>
2024-02-14 13:21:14 +0900

    [retry] 한 번 더 풀어보고 싶은 문제(맞음)

--- x/Main.java
+++ x/Main.java
@@ -9,14 +9,23 @@ class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		int[] arr = new int[n];
+		int[][] dots= new int[n][2];
+
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			int[] dot = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			dots[i][0] = dot[0];
+			dots[i][1] = dot[1];
 		}
 
-		Arrays.sort(arr);
+		Arrays.sort(dots, (o1, o2) -> {
+			if (o1[1] == o2[1]) {
+				return o1[0] - o2[0];
+			}
+			return o1[1] - o2[1];
+		});
+
 		for (int i = 0; i < n; i++) {
-			bw.append(arr[i] + "\n");
+			bw.append(dots[i][0] + " " + dots[i][1] + "\n");
 		}
 
 		br.close();
