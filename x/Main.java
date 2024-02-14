0b7d9f4e5ec174d0a2c2e0e63708c3ce32bef0e8
ebcode2021 <ebcode2021@gmail.com>
2024-02-14 15:35:00 +0900

    [retry] 스택이라 룰루랄라 풀다가 조건 잘못 읽어서 고생함..

--- x/Main.java
+++ x/Main.java
@@ -3,33 +3,55 @@ import java.util.*;
 
 class Main {
 
+	private static boolean isOpenBrace(char c) {
+		if (c == '(' || c == '[')
+			return true;
+		return false;
+	}
+
+	private static boolean isCloseBrace(char c) {
+		if (c == ']' || c == ')')
+			return true;
+		return false;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
 
-		int n = Integer.parseInt(br.readLine());
-
-		int[][] dots= new int[n][2];
-
-		for (int i = 0; i < n; i++) {
-			int[] dot = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			dots[i][0] = dot[0];
-			dots[i][1] = dot[1];
-		}
-
-		Arrays.sort(dots, (o1, o2) -> {
-			if (o1[1] == o2[1]) {
-				return o1[0] - o2[0];
+		while(true) {
+			char[] input = br.readLine().toCharArray();
+			
+			if (input.length == 1 && input[0] == '.') {
+				break;
 			}
-			return o1[1] - o2[1];
-		});
+			
+			Deque<Character> stack = new ArrayDeque<>();
+			int idx = 0;
+			boolean flag = true;
 
-		for (int i = 0; i < n; i++) {
-			bw.append(dots[i][0] + " " + dots[i][1] + "\n");
+			while (idx < input.length) {
+				char current = input[idx];
+				if (isOpenBrace(current) == true) {
+					stack.push(current);
+				} else if (isCloseBrace(current) == true) {
+					if (stack.isEmpty()) {
+						flag = false;
+						break ;
+					}
+					char pop = stack.pop();
+					if (!((pop == '(' && current == ')') || (pop == '[' && current == ']'))) {
+						flag = false;
+						break ;
+					}
+				}
+				idx++;
+			}
+
+			if (!stack.isEmpty() || flag == false) {
+				System.out.println("no");
+			} else {
+				System.out.println("yes");
+			}
 		}
-
-		br.close();
-		bw.flush();
-		bw.close();
 	}
 }
