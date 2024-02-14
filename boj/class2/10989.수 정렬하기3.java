9fec218139d323b4e3e8a807a4047bf856eabbc7
ebcode2021 <ebcode2021@gmail.com>
2024-02-14 12:42:01 +0900

    [solve] 빡셌던 시간 초과

--- boj/class2/10989.수 정렬하기3.java
+++ boj/class2/10989.수 정렬하기3.java
@@ -5,25 +5,22 @@ class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
 
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		Arrays.sort(arr);
-
-		int value = (int) Math.round(n * 0.15);
-
-		double sum = 0;
-		for (int i = value; i < n - value; i++) {
-			sum += arr[i];
+		for (int i = 0; i < n; i++) {
+			bw.append(arr[i] + "\n");
 		}
 
-		System.out.println((int)Math.round(sum / ( n - 2 * value)));
-
 		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
--- x/Main.java
+++ x/Main.java
@@ -2,30 +2,25 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		List<int[]> list = new ArrayList<>();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
 
-		int dotCnt = Integer.parseInt(br.readLine());
-		for (int i = 0; i < dotCnt; i++) {
-			int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			list.add(nums);
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		list.sort((o1, o2) -> {
-			if (o1[0] == o2[0]) {
-				return o1[1] - o2[1];
-			}
-			return o1[0] - o2[0];
-		});
-
-		for(int[] data : list) {
-			bw.append(data[0] + " " + data[1] + "\n");
+		Arrays.sort(arr);
+		for (int i = 0; i < n; i++) {
+			bw.append(arr[i] + "\n");
 		}
 
-		bw.flush();
 		br.close();
+		bw.flush();
 		bw.close();
 	}
 }
