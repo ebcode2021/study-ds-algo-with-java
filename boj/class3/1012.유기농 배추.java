e52f7b11ee63a3fadb93778d547b15c7ef0a5414
ebcode2021 <ebcode2021@gmail.com>
2024-02-17 18:40:41 +0900

    [retry] row, col 기준 잘 잡아서 재귀 돌릴것

--- boj/class3/1012.유기농 배추.java
+++ boj/class3/1012.유기농 배추.java
@@ -15,6 +15,7 @@ class Main {
 		dfs(map, row, col - 1);
 		dfs(map, row, col + 1);
 	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
