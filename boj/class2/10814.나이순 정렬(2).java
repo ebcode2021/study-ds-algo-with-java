7a3044d02a93e4e2be26655ce12d144a04085b76
ebcode2021 <ebcode2021@gmail.com>
2024-10-01 23:01:42 +0900

    [solve] 최대한 기본 자료형을 사용하면 좋나?

--- boj/class2/10814.나이순 정렬(2).java
+++ boj/class2/10814.나이순 정렬(2).java
@@ -1,3 +1,35 @@
-public class 10814.나이순 정렬(2) {
-	
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+
+		int[] ageArr = new int[N];
+		String[] nameArr = new String[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			ageArr[i] = Integer.parseInt(st.nextToken());
+			nameArr[i] = st.nextToken();
+		}
+
+		int currentAge = 1;
+		int count = 0;
+		while (currentAge <= 200 && count < N) {
+			for (int i = 0; i < N; i++) {
+				if (ageArr[i] == currentAge) {
+					bw.write(ageArr[i] + " " + nameArr[i] + "\n");
+					count++;
+				}
+			}
+			currentAge++;
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
