9bbb78642c8bb0c78f40c610f060cb272591184e
ebcode2021 <ebcode2021@gmail.com>
2024-10-01 22:06:03 +0900

    [solve] 제약조건이 힌트가 되서 쉽게 품

---
+++ boj/class2/7568.덩치.java
@@ -0,0 +1,3 @@
+public class 7568.덩치 {
+	
+}
--- x/Main.java
+++ x/Main.java
@@ -1,22 +1,38 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 키, 몸무게가 모두 커야 덩치가 더 큰 사람으로 인정
+ * (x, y) , (p, q) -> x > p, y > q 여야 성립
+ * 범위가 적은 걸로 보아 브루트포스
+ */
 class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 
-		int[] arr = new int[N];
+		int[][] arr = new int[N][2];
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(arr);
-
+		int[] rankArr = new int[N];
 		for (int i = 0; i < N; i++) {
-			bw.write(arr[i] + "\n");
+			int ranking = 1;
+			for (int j = 0; j < N; j++) {
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					ranking++;
+				}
+			}
+			rankArr[i] = ranking;
+		}
+
+		for (int i = 0; i< N; i++) {
+			bw.write(rankArr[i] + " ");
 		}
 
 		bw.flush();
