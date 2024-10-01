5e7f451b27285cae81add512022839d1bdcd8508
ebcode2021 <ebcode2021@gmail.com>
2024-10-01 23:01:36 +0900

    [solve] 최대한 기본 자료형을 사용하면 좋나?

--- x/Main.java
+++ x/Main.java
@@ -1,41 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 키, 몸무게가 모두 커야 덩치가 더 큰 사람으로 인정
- * (x, y) , (p, q) -> x > p, y > q 여야 성립
- * 범위가 적은 걸로 보아 브루트포스
- */
 class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+		
 		int N = Integer.parseInt(br.readLine());
 
-		int[][] arr = new int[N][2];
+		int[] ageArr = new int[N];
+		String[] nameArr = new String[N];
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			ageArr[i] = Integer.parseInt(st.nextToken());
+			nameArr[i] = st.nextToken();
 		}
 
-		int[] rankArr = new int[N];
-		for (int i = 0; i < N; i++) {
-			int ranking = 1;
-			for (int j = 0; j < N; j++) {
-				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-					ranking++;
+		int currentAge = 1;
+		int count = 0;
+		while (currentAge <= 200 && count < N) {
+			for (int i = 0; i < N; i++) {
+				if (ageArr[i] == currentAge) {
+					bw.write(ageArr[i] + " " + nameArr[i] + "\n");
+					count++;
 				}
 			}
-			rankArr[i] = ranking;
-		}
-
-		for (int i = 0; i< N; i++) {
-			bw.write(rankArr[i] + " ");
+			currentAge++;
 		}
 
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
