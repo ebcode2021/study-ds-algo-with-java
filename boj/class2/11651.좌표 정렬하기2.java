10e84bce25c08c65f1581d35469c3f6adf49ff3b
ebcode2021 <ebcode2021@gmail.com>
2024-10-16 00:07:12 +0900

    [solve] stream 함부로 사용하면 메모리 사용량이 쭉쭉 올라감

--- boj/class2/11651.좌표 정렬하기2.java
+++ boj/class2/11651.좌표 정렬하기2.java
@@ -1,3 +1,34 @@
-public class 11651.좌표 정렬하기2 {
-	
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int num = Integer.parseInt(br.readLine());
+
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < num; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list.add(new int[]{x, y});
+		}
+
+		Collections.sort(list, (l1, l2) -> {
+			if (l1[1] == l2[1]) {
+				return l1[0] - l2[0];
+			}
+			return l1[1] - l2[1];
+		});
+
+		for (int[] a : list) {
+			bw.write(a[0] + " " + a[1] + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
