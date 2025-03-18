c94283189be8c8d4ab50382227ec35220dc23d46
ebcode2021 <ebcode2021@gmail.com>
2025-03-18 23:43:58 +0900

    [solve] O(n^2) -> O(nlogn)

--- study_2025/56.Merge Intervals.java
+++ study_2025/56.Merge Intervals.java
@@ -29,7 +29,7 @@ class Solution {
             for (int j = i + 1; j < intervals.length; j++) {
                 // 뒤의 시작점이 앞의 도착점보다 작은 값이 아닐때
                 if (!(checkIntervals[i][1] >= checkIntervals[j][0])) {
-                    i = j - 1;
+                    i = j - 1; // 병합된 부분은 이후 다시 비교하지 않음
                     break;
                 } else {
                     checkIntervals[i][1] = Math.max(checkIntervals[i][1], checkIntervals[j][1]);
@@ -43,5 +43,32 @@ class Solution {
                      .filter(interval -> interval[2] == 0)
                      .map(interval -> new int[]{interval[0], interval[1]})
                      .toArray(int[][]::new);
+    }
+}
+
+/**
+ * Solve 2 : O(nlogn)
+ * Try : 3
+ * Result : Runtime(8ms, Beats 86.84%), Memory(46.77MB, Beats : 38.89%)
+ */
+class Solution2 {
+    public int[][] merge(int[][] intervals) {
+        // 1. intervals 정렬 -> 뒤의 요소까지 비교할 필요 X
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // 2.checked가 포함된 3차원 배열 생성(unchecked -> -1) -> 리스트 변경
+        List<int[]> intervalList = new ArrayList<>();
+
+        // 3. O(n^2)로 순회 -> O(n)로 순회
+        for (int[] interval : intervals) {
+            if (intervalList.isEmpty() || intervalList.get(intervalList.size() - 1)[1] < interval[0]) {
+                intervalList.add(interval);
+            } else {
+                intervalList.get(intervalList.size() - 1)[1] = Math.max(intervalList.get(intervalList.size() - 1)[1], interval[1]);
+            }
+        }
+        
+        // 4. 새로운 배열 반환 -> 2에서 만든 배열 반환
+        return intervalList.toArray(new int[intervalList.size()][]);
     }
 }
