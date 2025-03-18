38220e0feaf83862beb1a1dec0756b7e87d6cb63
ebcode2021 <ebcode2021@gmail.com>
2025-03-18 23:51:02 +0900

    [solve] Memory 38% -> 83%

--- study_2025/56.Merge Intervals.java
+++ study_2025/56.Merge Intervals.java
@@ -16,7 +16,7 @@ class Solution {
             return Integer.compare(a[0], b[0]);
         });
 
-        // 2.checked가 포함된 3차원 배열 생성(unchecked -> -1)
+        // 2.checked가 포함된 3차원 배열 생성(unchecked = -1)
         int[][] checkIntervals = new int[intervals.length][3];
         for (int i = 0; i < intervals.length; i++) {
             checkIntervals[i][0] = intervals[i][0];
@@ -48,7 +48,7 @@ class Solution {
 
 /**
  * Solve 2 : O(nlogn)
- * Try : 3
+ * Try : 1
  * Result : Runtime(8ms, Beats 86.84%), Memory(46.77MB, Beats : 38.89%)
  */
 class Solution2 {
@@ -56,7 +56,7 @@ class Solution2 {
         // 1. intervals 정렬 -> 뒤의 요소까지 비교할 필요 X
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // 2.checked가 포함된 3차원 배열 생성(unchecked -> -1) -> 리스트 변경
+        // 2.checked가 포함된 3차원 배열 생성(unchecked = -1) -> 리스트 변경
         List<int[]> intervalList = new ArrayList<>();
 
         // 3. O(n^2)로 순회 -> O(n)로 순회
@@ -68,7 +68,35 @@ class Solution2 {
             }
         }
         
-        // 4. 새로운 배열 반환 -> 2에서 만든 배열 반환
+        // 4. 새로운 배열 반환 -> 2에서 만든 리스트를 배열로 반환
         return intervalList.toArray(new int[intervalList.size()][]);
+    }
+}
+
+/**
+ * Solve 3 : O(nlogn)
+ * Try : 1
+ * Result : Runtime(8ms, Beats 86.84%), Memory(46.45MB, Beats : 83.63%)
+ */
+class Solution3 {
+    public int[][] merge(int[][] intervals) {
+        // 1. intervals 정렬 -> 뒤의 요소까지 비교할 필요 X
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // 2.checked가 포함된 3차원 배열 생성(unchecked -> -1) -> 리스트 변경 -> 배열 생성
+        int[][] result = new int[intervals.length][2];
+        int idx = 0;
+
+        // 3. O(n^2)로 순회 -> O(n)로 순회
+        for (int[] interval : intervals) {
+            if (idx == 0 || result[idx - 1][1] < interval[0]) {
+                result[idx++] = interval;
+            } else {
+                result[idx - 1][1] = Math.max(result[idx - 1][1], interval[1]);
+            }
+        }
+        
+        // 4. 새로운 배열 반환 -> 2에서 만든 리스트를 배열로 반환 -> copyOf() 사용
+        return Arrays.copyOf(result, idx);
     }
 }
