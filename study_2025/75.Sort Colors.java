ac87da283687f810518e51aaf96a57fd58648a18
ebcode2021 <ebcode2021@gmail.com>
2025-04-09 21:16:03 +0900

    [solve] 한줄!

---
+++ study_2025/75.Sort Colors.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+/**
+ * Solve 1 : O(nlogn)
+ * Wrong Cnt : 0
+ * Result : Runtime(0ms, Beats 100%), Memory(42.28MB, Beats : 17.74%)
+ */
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
