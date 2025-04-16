7314d196c8609cd4d67e78de4e0131c4819e2936
ebcode2021 <ebcode2021@gmail.com>
2025-04-16 23:03:58 +0900

    [retry] SC, TC 추가

--- study_2025/819.Most Common Word.java
+++ study_2025/819.Most Common Word.java
@@ -26,3 +26,31 @@ class Solution {
         return answer;
     }
 }
+
+// TC, SC
+/**
+    paragraph -> N
+    banned -> K
+    words -> M
+ */
+class Solution2 { // O(2n + 2k + m) -> O(N) + O(K)
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String[] words = paragraph.toLowerCase().split("[^a-z]+"); // TC : 2n, SC : m 
+        Map<String, Integer> map = new HashMap<>(); 
+        Set<String> bannedSet = new HashSet<>(Arrays.asList(banned)); // TC : O(2k), SC : O(2k)
+        
+        int max = 0;
+        String answer = "";
+        for (int i = 0; i < words.length; i++) { // TC : O(m)
+            if (bannedSet.contains(words[i])) {  // SC : O(1)
+                continue;
+            }
+            map.put(words[i], map.getOrDefault(words[i], 0) + 1); // O(1)
+            if (max < map.get(words[i])) { // ** Collections.max 를 통해 return 시 한 번만
+                max = map.get(words[i]);
+                answer = words[i];
+            }
+        }
+        return answer;
+    }
+}
